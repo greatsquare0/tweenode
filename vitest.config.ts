26026dc18d00cda2c48ccc726347a263c9e251a0
greatsquare0 <greatsquare0@proton.me>
2024-11-08 17:50:56 -0300

    Hot fix to CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    setupFiles: ['./__tests__/vitest.setup.ts'],
+    //setupFiles: ['./__tests__/vitest.setup.ts'],
     testTimeout: process.platform == 'win32' ? 50000 : 10000,
     hookTimeout: process.platform == 'win32' ? 50000 : 10000,
   },
